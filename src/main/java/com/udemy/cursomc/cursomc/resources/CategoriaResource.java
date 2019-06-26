908e995757de02ccb0cbd08a3f283e44e601bb21
Fernando Furbringer <fernando.furbringer@gmail.comemail>
2019-06-26 18:14:59 -0300

    Criando categoria

--- src/main/java/com/udemy/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,12 @@
 package com.udemy.cursomc.cursomc.resources;
 
+import com.udemy.cursomc.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
@@ -10,8 +14,16 @@ public class CategoriaResource {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest ta funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 
