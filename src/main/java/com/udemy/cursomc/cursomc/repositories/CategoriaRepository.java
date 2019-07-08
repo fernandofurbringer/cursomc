69506cdb8f59d77436250634a04c84ab03f2678c
Fernando Furbringer <fernando.furbringer@gmail.comemail>
2019-07-08 11:08:02 -0300

    Criando operação de instanciação

---
+++ src/main/java/com/udemy/cursomc/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.udemy.cursomc.cursomc.repositories;
+
+import com.udemy.cursomc.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
--- src/main/java/com/udemy/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,10 @@
 package com.udemy.cursomc.cursomc.resources;
 
 import com.udemy.cursomc.cursomc.domain.Categoria;
+import com.udemy.cursomc.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +16,14 @@ import java.util.List;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
+    @Autowired
+    private CategoriaService service;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar(){
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id){
 
-        Categoria cat1 = new Categoria(1,"Informática");
-        Categoria cat2 = new Categoria(2,"Escritório");
-
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 
